a94a0817a8b37c61783304bb61f4af0b60a7f58f
JDAM <jdam50002@gmail.com>
2024-01-23 23:58:17 -0400

    feat(collection-sec) isotope.js was added and also was finished this sec (style and filtering)

--- js/app.js
+++ js/app.js
@@ -8,4 +8,22 @@ window.addEventListener('scroll', () => {
     }
 })
 
+// init Isotope
+var $grid = $('.collection-list').isotope({
+    // options
+  })
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns()
+    $(this).addClass("active-filter-btn")
+    $grid.isotope({ filter: filterValue });
+  })
 
+  var filterBtns = $(".filter-button-group").find('button')
+  console.log(filterBtns)
+  function resetFilterBtns () {
+    filterBtns.each(function() {
+        $(this).removeClass("active-filter-btn")
+    })
+  }
